885214ba2139aa2b047b77a4434fcdce40a45d41
Amir Hurtado <a.hurtado@utp.edu.co>
2026-05-15 22:29:46 -0500

    Add output variables for EC2 instance ID and public IP

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -2,3 +2,8 @@ output "public_ip_ec2_instance" {
     value = module.ec2_instance.public_ip
     description = "The public IP address of the EC2 instance"
 }
+
+output "id_ec2_instance" {
+    value = module.ec2_instance.id
+    description = "The ID of the EC2 instance"
+}
